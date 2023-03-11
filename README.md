88cf3f1e6a3c05cea9113bc6334718257f7c706f
Danilo Camargo <danilo.omnis@gmail.com>
2023-03-11 08:56:12 -0300

    edicao do arquivo readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # cbcrestapi
+
+- Edite os arquivos conexao.php e o criarbanco.php com os dados de conexão do banco de dados
+
+- execute a url http://localhost/cbcrestapi/criarbanco.php
+
+- abra a url http://localhost/cbcrestapi
+
--- criarbanco.php
+++ criarbanco.php
@@ -6,24 +6,19 @@ $username = "root";
 $password = "";
 $dbname = "cbc_db";
 
-// Cria a conexão com o banco de dados
 $conn = new mysqli($servername, $username, $password);
 
-// Verifica se a conexão foi estabelecida com sucesso
 if ($conn->connect_error) {
     die("Erro ao conectar ao banco de dados: " . $conn->connect_error);
 }
 
-// Cria o banco de dados "seu_banco_de_dados" se ainda não existir
 $sql = "CREATE DATABASE IF NOT EXISTS $dbname";
 if ($conn->query($sql) === FALSE) {
     die("Erro ao criar o banco de dados: " . $conn->error);
 }
 
-// Selecione o banco de dados
 $conn->select_db($dbname);
 
-// Cria a tabela "clubes" com as colunas "id" , "clube" e "saldo_disponivel"
 $sql = "CREATE TABLE clubes (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 clube VARCHAR(30) NOT NULL,
@@ -33,7 +28,6 @@ if ($conn->query($sql) === FALSE) {
     die("Erro ao criar a tabela: " . $conn->error);
 }
 
-// Cria a tabela "recursos" com as colunas "id" , "recurso" e "saldo_disponivel"
 $sql = "CREATE TABLE recursos (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 recurso VARCHAR(30) NOT NULL,
@@ -51,7 +45,6 @@ mysqli_query($conn, $sql);
 
 echo "Banco de dados e tabela criados com sucesso!";
 
-// Fecha a conexão com o banco de dados
 $conn->close();
 
 ?>
